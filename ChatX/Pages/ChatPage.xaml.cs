7f70dc8e27cb4d81744deb89d7492f8c2faed9e9
Script Devil <71324001+Benceplays@users.noreply.github.com>
2023-07-22 23:56:38 +0200

    par cucc

--- ChatX/Pages/ChatPage.xaml.cs
+++ ChatX/Pages/ChatPage.xaml.cs
@@ -5,6 +5,20 @@ public partial class ChatPage : ContentPage
 	public ChatPage()
 	{
 		InitializeComponent();
+		CodeGenerate();
+
+    }
+	public void CodeGenerate()
+	{
+		int code;
+		Random random = new Random();
+		code = random.Next(1000, 100000);
+		codeLabel.Text = $"Code: {code}";
+
+    }
+	public void sendButton(object sender, EventArgs e)
+	{
+		string message = messagetext.Text;
 	}
 
 }
